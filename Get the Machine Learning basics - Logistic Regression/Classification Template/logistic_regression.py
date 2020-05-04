beda9f9bd50f5b4f63987b9779cbe46fecd3d823
Menosse <famcsn2@gmail.com>
2020-05-04 18:15:23 -0300

    deep

--- Get the Machine Learning basics - Logistic Regression/Classification Template/logistic_regression.py
+++ Get the Machine Learning basics - Logistic Regression/Classification Template/logistic_regression.py
@@ -64,7 +64,7 @@ y = dataset.iloc[:, 4].values
 
 
 # ### ================================= ###
-# ### 5 - Create a training and test set
+# ### 5 - Split a training and test set
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.25, random_state = 0)
